3136de2ac224841632c257bf365be8950e8556fb
Le Rong Shi <larry.shi@mail.utoronto.ca>
2019-11-17 21:34:25 -0500

    added call to keyboard library

--- pong.v
+++ pong.v
@@ -1,7 +1,16 @@
+`include "PS2MouseKeyboard/PS2_Keyboard_Controller.v"
+`include "vga_adaptor/vga_adaptor.v"
 
 module pong
 	(
 		CLOCK_50,						//	On Board 50 MHz
 	);
+
+endmodule
+
+
+module keyboard();
+
+	keyboard_tracker #(.PULSE_OR_HOLD(1)) k1();
 
 endmodule
