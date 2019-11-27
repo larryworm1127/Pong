a3538d9a5691bbd68df570f7bce63833a5e8a01c
Le Rong Shi <larry.shi@mail.utoronto.ca>
2019-11-26 19:24:19 -0500

    updated top level module

--- pong.v
+++ pong.v
@@ -1,5 +1,3 @@
-`include "PS2MouseKeyboard/PS2_Keyboard_Controller.v"
-`include "vga_adaptor/vga_adaptor.v"
 
 module pong
 	(
@@ -47,7 +45,13 @@ module pong
 	// Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
 	// image file (.MIF) for the controller.
-	vga_adapter VGA(
+	vga_adapter # (
+		.RESOLUTION             ("320x240"),
+		.MONOCHROME             ("FALSE"),
+		.BITS_PER_COLOUR_CHANNEL(1),
+		.BACKGROUND_IMAGE       ("background.mif");
+		)
+		VGA(
 			.resetn   (resetn),
 			.clock    (CLOCK_50),
 			.colour   (colour),
@@ -83,33 +87,93 @@ module pong
 	);
 
 	// Instantiate datapath
-	wire paddle1_erase; 
+	// wire paddle1_erase; 
 	
-	datapath paddle1(
-		.x_in      (0),
-		.y_in      (0),
-		.resetn    (resetn),
-		.enable    (writeEn),
-		.clk       (CLOCK_50),
-		.down      (key_down),
-		.up        (key_up),
-		.left      (key_left),
-		.right     (key_right),
-		.x_out     (x),
-		.y_out     (y),
-		.do_erase  (paddle1_erase)
-	);
+	// datapath paddle1(
+	// 	.x_in      (0),
+	// 	.y_in      (0),
+	// 	.resetn    (resetn),
+	// 	.enable    (writeEn),
+	// 	.clk       (CLOCK_50),
+	// 	.down      (key_down),
+	// 	.up        (key_up),
+	// 	.left      (key_left),
+	// 	.right     (key_right),
+	// 	.x_out     (x),
+	// 	.y_out     (y),
+	// 	.do_erase  (paddle1_erase)
+	// );
 
 	// Instantiate FSM control
-	control paddleControl1(
-		.clk      (CLOCK_50),
-		.resetn   (resetn),
-		.go       (paddle1_erase),
-		.plot     (plot),
-		.enable   (writeEn),
-		.load_en  (load_en),
-		.colourOut(colour)
-	);
+	// control paddleControl1(
+	// 	.clk      (CLOCK_50),
+	// 	.resetn   (resetn),
+	// 	.go       (paddle1_erase),
+	// 	.plot     (plot),
+	// 	.enable   (writeEn),
+	// 	.load_en  (load_en),
+	// 	.colourOut(colour)
+	// );
+	
+	locationProcessor # (
+      .BOX_WIDTH              (9'd4),
+      .BOX_HEIGHT              (9'd4),
+      .SCREEN_WIDTH              (9'd160),
+      .SCREEN_HEIGHT                (9'd120),
+      .FRAME_RATE_COUNT       (32'd9999999) //5 Hz
+      )
+      processor (
+            .clock (clock),
+            .reset_n (reset_n),
+            .in_color (color_in),
+            .m_ready(screen_drawer_ready),
+            .m_valid  (processor_data_valid),
+            .box_x (box_x),
+            .box_y    (box_y),
+            .out_color(box_color)
+            );
+
+    screenDrawer # (
+      .BOX_WIDTH              (9'd4),
+      .BOX_HEIGHT              (9'd4),
+      .SCREEN_WIDTH              (9'd160),
+      .SCREEN_HEIGHT                  (9'd120),
+      .REFRESH_RATE_COUNT       (32'd833332) //60Hz
+      )
+      screen_drawer_0 (
+            .clock (clock),
+            .reset_n (reset_n),
+            .s_ready (screen_drawer_ready),
+             .s_valid      (processor_data_valid),
+             .in_box_x     (box_x),
+             .in_box_y     (box_y),
+             .in_box_color (box_color),
+
+             .m_ready (box_drawer_ready),
+             .m_valid (screen_drawer_data_valid),
+             .out_box_x    (draw_box_x),
+             .out_box_y    (draw_box_y),
+             .out_box_w    (draw_box_w),
+             .out_box_h    (draw_box_h),
+             .out_box_color (draw_box_color)
+            );
+
+    boxDrawer box_drawer_0 (
+            .clock       (clock),
+            .reset_n    (reset_n),
+
+            .s_ready     (box_drawer_ready),
+            .s_valid     (screen_drawer_data_valid),
+            .in_box_x    (draw_box_x),
+            .in_box_y    (draw_box_y),
+            .in_box_w    (draw_box_w),
+            .in_box_h    (draw_box_h),
+            .in_box_color(draw_box_color),
+            .vga_x       (vga_x),
+            .vga_y       (vga_y),
+            .plot            (vga_plot),
+            .colour      (vga_color)
+      );
 
 	
 endmodule
